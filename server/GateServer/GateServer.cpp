347eb5dc73cdff0dd4272a00f6166b80787df8fe
shlock7 <1397277295@qq.com>
2024-10-26 16:08:39 +0800

    feat: config Redis, package RedisMgr class

--- server/GateServer/GateServer.cpp
+++ server/GateServer/GateServer.cpp
@@ -2,11 +2,138 @@
 #include <json/json.h>
 #include <json/value.h>
 #include <json/reader.h>
+#include <cassert>
 #include "CServer.h"
 #include "ConfigMgr.h"
+#include "hiredis.h"
+#include "RedisMgr.h"
+
+// 测试redis连接
+void TestRedis()
+{
+    // 连接redis 需要启动才可以进行连接
+    // redis默认监听端口为6387 可以再配置文件中修改
+    //redisContext* c = redisConnect("81.68.86.146", 6380);
+    redisContext* c = redisConnect("127.0.0.1", 6380);
+    if (c->err)
+    {
+        printf("Connect to redisServer faile:%s\n", c->errstr);
+        redisFree(c);        return;
+    }
+    printf("Connect to redisServer Success\n");
+
+    std::string redis_password = "123456";
+    // redis命令，带参数
+    redisReply* r = (redisReply*)redisCommand(c, "AUTH %s", redis_password.c_str());
+    if (r->type == REDIS_REPLY_ERROR)
+    {
+        printf("Redis认证失败！\n");
+    }
+    else
+    {
+        printf("Redis认证成功！\n");
+    }
+
+    //为redis设置key
+    const char* command1 = "set stest1 value1";
+
+    //执行redis命令行
+    r = (redisReply*)redisCommand(c, command1);
+
+    //如果返回NULL则说明执行失败
+    if (NULL == r)
+    {
+        printf("Execut command1 failure\n");
+        redisFree(c);        return;
+    }
+
+    //如果执行失败则释放连接
+    if (!(r->type == REDIS_REPLY_STATUS && (strcmp(r->str, "OK") == 0 || strcmp(r->str, "ok") == 0)))
+    {
+        printf("Failed to execute command[%s]\n", command1);
+        freeReplyObject(r);
+        redisFree(c);        return;
+    }
+
+    //执行成功 释放redisCommand执行后返回的redisReply所占用的内存
+    freeReplyObject(r);
+    printf("Succeed to execute command[%s]\n", command1);
+
+    const char* command2 = "strlen stest1";
+    r = (redisReply*)redisCommand(c, command2);
+
+    //如果返回类型不是整形 则释放连接
+    if (r->type != REDIS_REPLY_INTEGER)
+    {
+        printf("Failed to execute command[%s]\n", command2);
+        freeReplyObject(r);
+        redisFree(c);        return;
+    }
+
+    //获取字符串长度
+    int length = r->integer;
+    freeReplyObject(r);
+    printf("The length of 'stest1' is %d.\n", length);
+    printf("Succeed to execute command[%s]\n", command2);
+
+    //获取redis键值对信息
+    const char* command3 = "get stest1";
+    r = (redisReply*)redisCommand(c, command3);
+    if (r->type != REDIS_REPLY_STRING)
+    {
+        printf("Failed to execute command[%s]\n", command3);
+        freeReplyObject(r);
+        redisFree(c);        return;
+    }
+    printf("The value of 'stest1' is %s\n", r->str);
+    freeReplyObject(r);
+    printf("Succeed to execute command[%s]\n", command3);
+
+    const char* command4 = "get stest2";
+    r = (redisReply*)redisCommand(c, command4);
+    if (r->type != REDIS_REPLY_NIL)
+    {
+        printf("Failed to execute command[%s]\n", command4);
+        freeReplyObject(r);
+        redisFree(c);        return;
+    }
+    freeReplyObject(r);
+    printf("Succeed to execute command[%s]\n", command4);
+
+    //释放连接资源
+    redisFree(c);
+
+}
+
+// redis封装测试用例
+void TestRedisMgr()
+{
+    assert(RedisMgr::GetInstance()->Connect("81.68.86.146", 6380));
+    assert(RedisMgr::GetInstance()->Auth("123456"));
+    assert(RedisMgr::GetInstance()->Set("blogwebsite", "llfc.club"));
+    std::string value = "";
+    assert(RedisMgr::GetInstance()->Get("blogwebsite", value));
+    assert(RedisMgr::GetInstance()->Get("nonekey", value) == false);
+    assert(RedisMgr::GetInstance()->HSet("bloginfo", "blogwebsite", "llfc.club"));
+    assert(RedisMgr::GetInstance()->HGet("bloginfo", "blogwebsite") != "");
+    assert(RedisMgr::GetInstance()->ExistsKey("bloginfo"));
+    assert(RedisMgr::GetInstance()->Del("bloginfo"));
+    assert(RedisMgr::GetInstance()->Del("bloginfo"));
+    assert(RedisMgr::GetInstance()->ExistsKey("bloginfo") == false);
+    assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue1"));
+    assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue2"));
+    assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue3"));
+    assert(RedisMgr::GetInstance()->RPop("lpushkey1", value));
+    assert(RedisMgr::GetInstance()->RPop("lpushkey1", value));
+    assert(RedisMgr::GetInstance()->LPop("lpushkey1", value));
+    assert(RedisMgr::GetInstance()->LPop("lpushkey2", value) == false);
+    RedisMgr::GetInstance()->Close();
+}
 
 int main()
 {
+    //TestRedis();
+    //TestRedisMgr();
     auto& gCfgMgr = ConfigMgr::Inst();
     std::string gate_port_str = gCfgMgr["GateServer"]["Port"];
     unsigned short gate_port = atoi(gate_port_str.c_str());
